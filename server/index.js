9a8e65b4822c9d10aa1f81b1bda56f06b6351008
José Moya Gómez <jmoya@grupotrisan.com>
2026-04-07 08:52:13 -0600

    configura cors para vercel

--- server/index.js
+++ server/index.js
@@ -12,7 +12,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://app-recetas-d7ej.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "20mb" }));
 
 const PORT = process.env.PORT || 3001;
